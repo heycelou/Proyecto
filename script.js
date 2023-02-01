f563bf2129f9d2066fb9bb570cdf09a87a77e6b5
heyce <heyce_1291@hotmail.com>
2023-02-01 11:39:22 -0300

    proyecto

---
+++ script.js
@@ -0,0 +1,40 @@
+let menuVisible = false;
+//funcion q oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible= false;
+    }else{
+        document.getElementById("nav").classList ="responsive"
+        menuVisible= true;
+    }
+}
+function seleccionar(){
+    //ocultar menu cuando selecciono una opcion
+    document.getElementById("nav").classList ="";
+    menuVisible= false;
+}
+//funcion que se aplica a las animaciones en seccion de habilidades
+function efectohabilidades(){
+    var skills= document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >=300){
+        let habilidades = document.getElementsByClassName("progreso")
+        habilidades [0].classList.add("javascript");
+        habilidades [1].classList.add("htmlcss");
+        habilidades [2].classList.add("phtoshop");
+        habilidades [3].classList.add("wordpress");
+        habilidades [4].classList.add("drupal");
+        habilidades [5].classList.add("comunicacion");
+        habilidades [6].classList.add("trabajo");
+        habilidades [7].classList.add("creatividad");
+        habilidades [8].classList.add("dedicacion");
+        habilidades [9].classList.add("proyect");
+    }
+}
+
+// animacion
+window.onscroll =function(){
+    efectohabilidades();
+}
+
